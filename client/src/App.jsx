b51ad20cea81fe14d2f832167ba03f15a1770ff4
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-22 13:01:16 -0700

    added logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import { logo } from './assets';
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
-    <h1 className='text-xl font-bold'>
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <header 
+        className='w-full flex justify-between items-center
+                 bg-white sm:px-8 px-4 py-4 border-b
+                 border-b-[#e6ebf4]'
+      >
+        <Link to='/'>
+          <img src={logo} alt='logo' className='w-28 object-contain'/>
+        </Link>
+      </header>
+    </BrowserRouter>
   )
 }
 
