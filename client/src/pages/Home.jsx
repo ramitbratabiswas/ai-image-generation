d90fd53d858df426771fcb58a6de2b7b0d7b337b
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-22 14:41:55 -0700

    set up RenderCards placeholders

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,7 +50,17 @@ const Home = () => {
               </h2>
             )}
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
-              
+              {searchText ? (
+                <RenderCards
+                  data={[]}
+                  title='No posts found'
+                />
+              ) : (
+                <RenderCards 
+                  data={[]}
+                  title='No posts found'
+                />
+              )}
             </div>
           </>)
         }
