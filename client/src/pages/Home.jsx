52f18fb1226a695d6bcce6f700872fc09e19353d
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-22 14:38:17 -0700

    created RenderCards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,19 @@ import { useState, useEffect } from 'react';
 
 import { Loader, Card, FormField } from '../components';
 
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => (
+      <Card key={post._id} {...post} />
+    ))
+  }
+  return (
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
+      {title}
+    </h2>
+  )
+}
+
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
