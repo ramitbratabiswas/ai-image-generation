722a8c3f1b2ae35fcd280407eaff76375bac3672
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-08-18 18:56:51 -0700

    wrote startServer function

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect';
+
 dotenv.config();
 
 const app = express();
@@ -9,5 +11,18 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', async (req, res) => {
-  res.send('Hello from DALL_E!');
+  res.send('Hello!');
 })
+
+const startServer = async () => {
+
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
+
+startServer();
